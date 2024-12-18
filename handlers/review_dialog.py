15bdbba405f19c61d442cf65051d1343755e256d
Kadzutoo <argenhalmatov200511@gmail.com>
2024-12-19 02:01:13 +0600

    restorantreview

--- handlers/review_dialog.py
+++ handlers/review_dialog.py
@@ -13,42 +13,42 @@ class RestorantReview(StatesGroup):
     cleanliness_rating = State()
     extra_comments = State()
 
-# Начало FSM
+# Имя
 @review_router.callback_query(F.data == "review")
 async def start_review(callback: types.CallbackQuery, state: FSMContext):
     await callback.answer()
     await callback.message.answer("Как вас зовут?")
     await state.set_state(RestorantReview.name)
 
-# Получение имени
+# Номер или инстаграмм
 @review_router.message(RestorantReview.name)
 async def get_name(message: types.Message, state: FSMContext):
     await state.update_data(name=message.text)
     await message.answer("Ваш номер телефона или Instagram?")
     await state.set_state(RestorantReview.phone_or_instagram)
 
-# Получение телефона или Instagram
+# Качество еды
 @review_router.message(RestorantReview.phone_or_instagram)
 async def get_contact(message: types.Message, state: FSMContext):
     await state.update_data(phone_or_instagram=message.text)
     await message.answer("Как вы оцениваете качество еды? (от 1 до 5)")
     await state.set_state(RestorantReview.food_rating)
 
-# Оценка еды
+# Оценка чистоты
 @review_router.message(RestorantReview.food_rating)
 async def get_food_rating(message: types.Message, state: FSMContext):
     await state.update_data(food_rating=message.text)
     await message.answer("Как оцениваете чистоту заведения? (от 1 до 5)")
     await state.set_state(RestorantReview.cleanliness_rating)
 
-# Оценка чистоты
+# Комментарий
 @review_router.message(RestorantReview.cleanliness_rating)
 async def get_cleanliness_rating(message: types.Message, state: FSMContext):
     await state.update_data(cleanliness_rating=message.text)
     await message.answer("Есть ли у вас дополнительные комментарии?")
     await state.set_state(RestorantReview.extra_comments)
 
-# Дополнительные комментарии
+#
 @review_router.message(RestorantReview.extra_comments)
 async def get_extra_comments(message: types.Message, state: FSMContext):
     await state.update_data(extra_comments=message.text)
